2ac081e2a0aed0b7f269ce53ec233173f2c13b31
Akira <54892411+docxed@users.noreply.github.com>
2022-04-23 21:24:16 +0700

    feat retrieve

--- src/views/Retrieve.vue
+++ src/views/Retrieve.vue
@@ -1,21 +1,39 @@
 <template>
   <div>
-      <h2>Example Vite Crud</h2>
-      <hr>
-      <br><br>
-      <div class="text-center">
-          <h3>Menu on navbar</h3>
-      </div>
+    <h2>Retrieve</h2>
+    <hr />
+    <br /><br />
+    <div>
+      <h3>{{ JSON.stringify(covidData, null, 4) }}</h3>
+    </div>
   </div>
 </template>
 <script>
+import axios from "axios";
+
 export default {
-  name: 'Retrieve',
+  name: "Retrieve",
   data() {
     return {
-
-    }
-  }
-}
+      covidData: "",
+    };
+  },
+  methods: {
+    getData() {
+      axios
+        .get("https://covid19.ddc.moph.go.th/api/Cases/today-cases-all")
+        .then((response) => {
+          const data = response.data;
+          this.covidData = data[0];
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+  },
+  mounted() {
+    this.getData();
+  },
+};
 </script>
 <style scoped></style>
